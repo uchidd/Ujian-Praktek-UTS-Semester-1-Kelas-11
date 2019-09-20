3316e127970be29ed84d71cc57efca9d5e7c5bf6
uchidd <smk_mursyid@rbs.sch.id>
2019-09-20 19:37:13 +0700

    09:15, 20 September 2019

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
@@ -2,12 +2,14 @@ package id.uchidd.ujianpraktekutssemester1kelas11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_details_test);
+
     }
 }
