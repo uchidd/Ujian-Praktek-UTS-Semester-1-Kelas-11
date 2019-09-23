5e3dbd9ec99f9d324a802a30f8a481b2ab32ac1f
rkkhaerul <rkkhaerul@gmail.com>
2019-09-23 13:23:23 +0700

    Rizki Khaerul | @rkkhaerul

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
@@ -1,18 +1,37 @@
 package id.uchidd.ujianpraktekutssemester1kelas11;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.cvStart)
+    CardView cvStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        cvStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChooseBrandActivity.class));
+            }
+        });
 
         hideNavigationBar();
     }
+
     public void hideNavigationBar() {
 
         this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -22,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit Application")
+                .setMessage("Do You want to Exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
