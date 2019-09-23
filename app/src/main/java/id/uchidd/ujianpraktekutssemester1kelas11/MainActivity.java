08a1a4ec3a7645f665f13e2b9e7ed6b57001c61c
uchidd <smk_mursyid@rbs.sch.id>
2019-09-23 13:07:14 +0700

    09:15, 20 September 2019

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details_test);
+        setContentView(R.layout.activity_details);
 
         hideNavigationBar();
     }
