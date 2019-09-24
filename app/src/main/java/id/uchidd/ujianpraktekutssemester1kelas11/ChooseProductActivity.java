85c149d163ac2cb196361ba1960bb945574c1b5b
uchidd <smk_mursyid@rbs.sch.id>
2019-09-24 07:27:25 +0700

    09:15, 20 September 2019

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/ChooseProductActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/ChooseProductActivity.java
@@ -8,6 +8,6 @@ public class ChooseProductActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_product);
+        setContentView(R.layout.activity_buy);
     }
 }
