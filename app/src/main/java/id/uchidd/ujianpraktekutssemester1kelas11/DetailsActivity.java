a1c17d9b9d6dc0f8e8986f9c909401ddea112d1c
rkkhaerul <rkkhaerul@gmail.com>
2019-09-25 08:05:32 +0700

    Rizki Khaerul | @rkkhaerul

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/DetailsActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/DetailsActivity.java
@@ -2,17 +2,40 @@ package id.uchidd.ujianpraktekutssemester1kelas11;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivBackCP)
+    ImageView ivBackCP;
+    @BindView(R.id.ivDetails)
+    ImageView ivDetails;
+    @BindView(R.id.tvDetails1)
+    TextView tvDetails1;
+    @BindView(R.id.tvDetails2)
+    TextView tvDetails2;
+    @BindView(R.id.tvDetails3)
+    TextView tvDetails3;
+    @BindView(R.id.tvDetails4)
+    TextView tvDetails4;
+    @BindView(R.id.cvAddToCart)
+    CardView cvAddToCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        ButterKnife.bind(this);
 
         hideNavigationBar();
     }
+
     public void hideNavigationBar() {
 
         this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
