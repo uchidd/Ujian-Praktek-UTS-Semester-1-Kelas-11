1678c89942a3774159bfa37ad6f6f989101b942e
uchidd <smk_mursyid@rbs.sch.id>
2019-09-29 22:21:32 +0700

    09:15, 20 September 2019

--- app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/BuyActivity.java
+++ app/src/main/java/id/uchidd/ujianpraktekutssemester1kelas11/BuyActivity.java
@@ -204,7 +204,6 @@ public class BuyActivity extends AppCompatActivity {
             }
         });
 
-        hideNavigationBar();
     }
 
     private void pushData(String brand,
@@ -243,15 +242,15 @@ public class BuyActivity extends AppCompatActivity {
 
     }
 
-    public void hideNavigationBar() {
-
-        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
-    }
+//    public void hideNavigationBar() {
+//
+//        this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//
+//    }
 
 }
